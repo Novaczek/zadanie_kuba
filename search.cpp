ebdeae4065851582e7f26ef705986686a67daefb
Jakub Nowak <154355518+Novaczek@users.noreply.github.com>
2024-02-14 10:19:02 +0100

    search.cpp

--- search.cpp
+++ search.cpp
@@ -1,6 +1,7 @@
-//WYSZUKIWANIE LINIOWE//
 #include <iostream>
+#include <cmath> // dla funkcji sqrt
 using namespace std;
+
 int wyszukiwanie_liniowe(int t[], int size, int k) {
     for (int i = 0; i < size; i++) {
         if (k == t[i]) {
@@ -10,63 +11,15 @@ int wyszukiwanie_liniowe(int t[], int size, int k) {
     return -1; 
 }
 
-int main() {
-    int t[] = {23, 3, 4, 6, 14, 11, 20, 7, 30, 15, 9, 10};
-    int p;
-
-    cout << "Podaj liczbe:" << endl;
-    cin >> p;
-
-    int result = wyszukiwanie_liniowe(t, 12, p);
-
-    if (result >= 0) {
-        cout << "Liczba " << p << " zostala znaleziona pod indexem: " << result << endl;
-    } else {
-        cout << "Liczba " << p << " nie zostala odnaleziona" << endl;
-    }
-
-    return 0;
-}
-//WYSZUKIWANIE LINIOWE Z WARTOWNIKIEM//
-#include <iostream>
-using namespace std;
 int wyszukiwanie_liniowe_z_wart(int t[], int size, int k) {
-    t[size] = k;
-
-    int i = 0;
-    
-    while (t[i] != k) {
-        i++;
+    for (int i = 0; i < size; i++) {
+        if (k == t[i]) {
+            return i; 
+        }
     }
-
-    
-    if (i < size) {
-        return i;
-    } else {
-        return -1;
-    }
+    return -1; 
 }
 
-int main() {
-    int t[] = {23, 3, 4, 6, 14, 11, 20, 7, 30, 15, 9, 10};
-    int p;
-
-    cout << "Podaj liczbe:" << endl;
-    cin >> p;
-
-    int result = wyszukiwanie_liniowe_z_wart(t, 12, p);
-
-    if (result != -1) {
-        cout << "Liczba " << p << " zostala znaleziona pod indexem: " << result << endl;
-    } else {
-        cout << "Liczba " << p << " nie zostala odnaleziona" << endl;
-    }
-
-    return 0;
-}
-//WYSZUKIWANIE BINARNE//
-#include <iostream>
-using namespace std;
 int wyszukiwanie_binarne(int t[], int size, int k) {
     int l = 0;
     int h = size - 1;
@@ -86,33 +39,6 @@ int wyszukiwanie_binarne(int t[], int size, int k) {
 
     return -1; 
 }
-
-int main() {
-    int t[] = {23, 3, 11, 5, 9, 8, 43, 78, 2, 10, 4, 13}; 
-    int p;
-
-    cout << "Podaj liczbe: " << endl;
-    cin >> p;
-
-    int result = wyszukiwanie_binarne(t, 12, p);
-
-    if (result >= 0) {
-        cout << "Liczba " << p << " zostala znaleziona pod indeksem: " << result << endl;
-    } else {
-        cout << "Liczba " << p << " nie zostala odnaleziona" << endl;
-    }
-
-    return 0;
-}
-//WYSZUKIWANIE SKOKOWE//
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-//WYSZUKIWANIE SKOKOWE//
-#include <iostream>
-#include <cmath>
-using namespace std;
 
 int wyszukiwanie_skokowe(int t[], int size, int k) {
     int step = sqrt(size); 
@@ -142,26 +68,6 @@ int wyszukiwanie_skokowe(int t[], int size, int k) {
     return -1; 
 }
 
-int main() {
-    int t[] = {2, 3, 4, 5, 8, 9, 10, 11, 13, 23, 43, 78};
-    int p;
-
-    cout << "Podaj liczbę: " << endl;
-    cin >> p;
-
-    int result = wyszukiwanie_skokowe(t, 12, p);
-
-    if (result >= 0) {
-        cout << "Liczba " << p << " została znaleziona pod indeksem: " << result << endl;
-    } else {
-        cout << "Liczba " << p << " nie została odnaleziona" << endl;
-    }
-
-    return 0;
-}
-//WYSZUKIWANIE INTERPOLACYJNE//
-#include <iostream>
-using namespace std;
 int wyszukiwanie_interpolacyjne(int t[], int size, int k) {
     int l = 0; 
     int h = size - 1; 
@@ -174,7 +80,6 @@ int wyszukiwanie_interpolacyjne(int t[], int size, int k) {
             return -1; 
         }
 
-        
         int p = l + (((double)(h - l) / (t[h] - t[l])) * (k - t[l]));
 
         if (t[p] == k) {
@@ -197,9 +102,37 @@ int main() {
 
     cout << "Podaj liczbę: " << endl;
     cin >> p;
+    int result;
 
-    int result = wyszukiwanie_interpolacyjne(t, 12, p);
+    result = wyszukiwanie_liniowe(t, 12, p);
+    if (result >= 0) {
+        cout << "Liczba " << p << " została znaleziona pod indeksem: " << result << endl;
+    } else {
+        cout << "Liczba " << p << " nie została odnaleziona" << endl;
+    }
 
+    result = wyszukiwanie_liniowe_z_wart(t, 12, p);
+    if (result >= 0) {
+        cout << "Liczba " << p << " została znaleziona pod indeksem: " << result << endl;
+    } else {
+        cout << "Liczba " << p << " nie została odnaleziona" << endl;
+    }
+
+    result = wyszukiwanie_binarne(t, 12, p);
+    if (result >= 0) {
+        cout << "Liczba " << p << " została znaleziona pod indeksem: " << result << endl;
+    } else {
+        cout << "Liczba " << p << " nie została odnaleziona" << endl;
+    }
+
+    result = wyszukiwanie_skokowe(t, 12, p);
+    if (result >= 0) {
+        cout << "Liczba " << p << " została znaleziona pod indeksem: " << result << endl;
+    } else {
+        cout << "Liczba " << p << " nie została odnaleziona" << endl;
+    }
+
+    result = wyszukiwanie_interpolacyjne(t, 12, p);
     if (result >= 0) {
         cout << "Liczba " << p << " została znaleziona pod indeksem: " << result << endl;
     } else {
@@ -207,7 +140,5 @@ int main() {
     }
 
     return 0;
-}
-
-    return 0;
-}
+} wytlumacz kod
+        
